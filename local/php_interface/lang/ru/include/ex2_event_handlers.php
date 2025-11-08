8a2a9f6f486319306402cec86199afbe891d14b7
UshakovDev <ushako160696@gmail.com>
2025-11-09 00:39:28 +0900

    [ex2-630] Индексация элементов
    - добавлен обработчик search:BeforeIndex для инфоблока рецензий
    - при индексации рецензий к заголовку дописывается текст с классом автора
    - текстовое значение берём из UF_USER_CLASS через CUserFieldEnum

--- local/php_interface/lang/ru/include/ex2_event_handlers.php
+++ local/php_interface/lang/ru/include/ex2_event_handlers.php
@@ -2,3 +2,4 @@
 
 $MESS['EX2_590_PREVIEW_SHORT'] = 'Текст анонса слишком короткий: #LEN#';
 $MESS['EX2_620_CLASS_EMPTY'] = 'Класс пользователя не указан';
+$MESS['EX2_630_CLASS_TITLE'] = 'Класс: #CLASS#';
